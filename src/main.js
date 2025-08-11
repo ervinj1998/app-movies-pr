a02c645d388787cff1541594f397387bb9f515a2
Alarick <ervinjoel2323@gmail.com>
2025-08-11 00:35:24 -0500

    setting up development environment

---
+++ src/main.js
@@ -0,0 +1,2 @@
+
+console.log('API URL = https://;kajskjldadalkdjakda.com/v1?api_jey=' + API_KEY)
---
+++ src/secrets.example.js
@@ -0,0 +1,2 @@
+//crea tu propio archivo con el nombre secrets.js donde puedes almacenar tu propia apikey
+ const API_KEY = ''
